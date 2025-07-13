b5cd3f5bddccc9010f76f9327e426296da727c66
Sourabh Bhat <heySourabh@gmail.com>
2025-07-13 11:26:39 +0200

    Renamed example

--- src/in/spbhat/CompoundInterest.java
+++ src/in/spbhat/CompoundInterest.java
@@ -3,11 +3,11 @@ package in.spbhat;
 import in.spbhat.dcm.Computation;
 import in.spbhat.dcm.Variable;
 
-public class Main {
+public class CompoundInterest {
     public static void main(String[] args) {
         Variable P = new Variable("P", 2355);
         Variable N = new Variable("N", 5);
-        Variable R = new Variable("R", 10);
+        Variable R = new Variable("R", 8);
 
         Variable I = new Variable("I", new Computation(P, N, R) {
             @Override
@@ -16,15 +16,6 @@ public class Main {
                 Variable N = inputs[1];
                 Variable R = inputs[2];
                 return P.value() * Math.pow(1 + R.value() / 100, N.value());
-            }
-        });
-
-        Variable S = new Variable("S", 1);
-        R.setComputation(new Computation(S) {
-            @Override
-            public double compute() {
-                Variable S = inputs[0];
-                return 10 + S.value();
             }
         });
 
